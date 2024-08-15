b174ad4ae3808d36979ee7feaa9f73eeb301c854
Aayushi-Sinha <122288743+Aayushi-Sinha@users.noreply.github.com>
2024-08-15 22:56:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 
 import './App.css';
-
 import Home from './components/Home.js'
 import TryOn from './components/TryOn.js';
 import Login from './components/Login.js';
-
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeCoverDesign from './HomeCoverDesign';
 import OrderHistory from './OrderHistory';
 import OrderList from './OrderList';
-
 
 function App() {
   return (
@@ -30,18 +26,6 @@ function App() {
         
           </Router>
      
-
-function App() {
-  return (
-    <div className="App">
-      <Router>
-      <Routes>
-        <Route path="/" element={<HomeCoverDesign/>} />
-        <Route path="/order-history" element={<OrderHistory/>} />
-        <Route path="/order-list" element={<OrderList/>} />
-        </Routes>
-    </Router>
-
     </div>
 
     
