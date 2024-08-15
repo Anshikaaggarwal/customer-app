ece0348046a7eda539b0dda2ada988fc60918c57
Anshikaaggarwal <anshikaaggarwal02@gmail.com>
2024-08-15 23:15:24 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import './App.css';
-
-import Home from './components/Home.js'
-import TryOn from './components/TryOn.js';
-import Login from './components/Login.js';
-
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeCoverDesign from './HomeCoverDesign';
 import OrderHistory from './OrderHistory';
 import OrderList from './OrderList';
-
-
-function App() {
-  return (
-    <div className="App">
-      {/* <Home/> */}
-      {/* <TryOn/> */}  
-      {/* <Login/> */}
-        <Router>
-         
-            <Routes>           
-              <Route path="/login" element={<Login />} />
-              <Route path="/tryon" element={<TryOn />} />
-              <Route path="/" element={<HomeCoverDesign/>} />
-              <Route path="/order-history" element={<OrderHistory/>} />
-              <Route path="/order-list" element={<OrderList/>} />
-            </Routes>
-        
-          </Router>
-     
-
 function App() {
   return (
     <div className="App">
@@ -40,9 +13,8 @@ function App() {
         <Route path="/order-list" element={<OrderList/>} />
         </Routes>
     </Router>
-
     </div>
   );
 }
 
-export default App;
+export default App;
