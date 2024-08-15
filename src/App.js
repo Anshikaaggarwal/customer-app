edf5a5ae93995f6cd14d7802de7062511e2b6773
Anshikaaggarwal <anshikaaggarwal02@gmail.com>
2024-08-15 23:17:38 +0530

    made rendering changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import './App.css';
+import Home from './components/Home.js'
+import TryOn from './components/TryOn.js';
+import Login from './components/Login.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeCoverDesign from './HomeCoverDesign';
 import OrderHistory from './OrderHistory';
 import OrderList from './OrderList';
+
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Routes>
-        <Route path="/" element={<HomeCoverDesign/>} />
-        <Route path="/order-history" element={<OrderHistory/>} />
-        <Route path="/order-list" element={<OrderList/>} />
-        </Routes>
-    </Router>
+      {/* <Home/> */}
+      {/* <TryOn/> */}  
+      {/* <Login/> */}
+        <Router>
+         
+            <Routes>           
+              <Route path="/login" element={<Login />} />
+              <Route path="/tryon" element={<TryOn />} />
+              <Route path="/" element={<HomeCoverDesign/>} />
+              <Route path="/order-history" element={<OrderHistory/>} />
+              <Route path="/order-list" element={<OrderList/>} />
+            </Routes>
+        
+          </Router>
+     
     </div>
+
+    
   );
 }
 
-export default App;
+export default App;
